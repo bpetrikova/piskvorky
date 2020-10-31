eb7fa287192c55dc0cef4a2be3a635cd061fe861
Barbora Petříková <bb.petrikova@gmail.com>
2020-11-01 00:15:02 +0100

    another commit task 5

--- index.js
+++ index.js
@@ -32,16 +32,78 @@ for (let i = 0; i < buttons.length; i++) {
 const hraciPole = 10; // 10x10
 
 const getPosition = (field) => {
-  let fieldIndex = 0;
-  while (fieldIndex < buttons.length) {
-    if (field === buttons[fieldIndex]) {
-      break;
+  for (let i = 0; i < buttons.length; i++) {
+    if (field === buttons[i]) {
+      return {
+        row: Math.floor(i / hraciPole),
+        column: i % hraciPole,
+      };
     }
-    fieldIndex++;
+  }
+};
+// zjišťuji pozici
+
+const getField = (row, column) => buttons[row * 10 + column];
+// funkce která ukazuje příslušný prvek
+
+const getSymbol = (field) => {
+  if (field.classList.contains('board__field--cross')) {
+    return 'cross';
+  } else if (field.classList.contains('board__field--circle')) {
+    return 'circle';
+  }
+};
+
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1; // Jednička pro právě vybrané políčko
+  // Koukni doleva
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
   }
 
-  return {
-    row: Math.floor(fieldIndex / hraciPole),
-    column: fieldIndex % hraciPole,
-  };
+  // Koukni doprava
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  // Koukni nahoru
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  // Koukni dolu
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
 };
