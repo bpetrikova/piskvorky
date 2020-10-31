b36f119899fc377c596036a28ed65a368a888811
Barbora Petříková <bb.petrikova@gmail.com>
2020-10-31 23:41:36 +0100

    change from comments + first commit task 5

--- index.js
+++ index.js
@@ -11,17 +11,37 @@ const buttons = document.querySelectorAll('button');
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', (event) => {
     if (tah === 'circle') {
-      event.target.classList.toggle('board__field--circle');
-      hrac.innerHTML = `hraje:
-      <img class="c-ikon white" src="img/cross.svg" alt="křížek" />`;
-      tah = 'cross';
-      event.target.setAttribute('disabled', true);
+      event.target.classList.add('board__field--circle');
+      hrac.src = `img/cross.svg`;
+      hrac.alt = `hraje křížek`;
+      tah = `cross`;
+      event.target.disabled = true;
     } else if (tah === 'cross') {
-      event.target.classList.toggle('board__field--cross');
-      hrac.innerHTML = ` hraje:
-      <img class="c-ikon white" src="img/circle.svg" alt="kolečko" />`;
+      event.target.classList.add('board__field--cross');
+      hrac.src = `img/circle.svg`;
+      hrac.alt = `hraje kolečko`;
       tah = 'circle';
-      event.target.setAttribute('disabled', true);
+      event.target.disabled = true;
     }
+    console.log(getPosition(buttons[i]));
+
+    // vypisuje na jaký button se kliklo
   });
 }
+// js kód, který udává vítěze
+const hraciPole = 10; // 10x10
+
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < buttons.length) {
+    if (field === buttons[fieldIndex]) {
+      break;
+    }
+    fieldIndex++;
+  }
+
+  return {
+    row: Math.floor(fieldIndex / hraciPole),
+    column: fieldIndex % hraciPole,
+  };
+};
